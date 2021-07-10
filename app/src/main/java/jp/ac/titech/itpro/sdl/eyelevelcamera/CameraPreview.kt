e131fcd22dbe8707e7b22480c33e9045664fb81d
osumi <osumi@se.cs.titech.ac.jp>
2021-07-10 15:18:15 +0900

    add eyeLevel

--- app/src/main/java/jp/ac/titech/itpro/sdl/eyelevelcamera/CameraPreview.kt
+++ app/src/main/java/jp/ac/titech/itpro/sdl/eyelevelcamera/CameraPreview.kt
@@ -6,6 +6,7 @@ import android.os.Build
 import android.os.Handler
 import android.os.HandlerThread
 import android.util.AttributeSet
+import android.util.Log
 import android.util.Rational
 import android.view.SurfaceHolder
 import android.view.TextureView
@@ -20,10 +21,11 @@ class CameraPreview : TextureView {
     }
     constructor(context: Context, attrs: AttributeSet): super(context, attrs) {
     }
-    constructor(context: Context, attrs: AttributeSet, defstyle: Int): super(context, attrs, defstyle) {
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int): super(context, attrs, defStyle) {
     }
 
-    fun setAspectRatio(width: Int, height: Int) {
+    fun setAspectRatio(width: Int?, height: Int?) {
+        Log.d(this::class.simpleName, "width = " +width+ ", height = " +height)
         widthRatio = width
         heightRatio = height
         requestLayout()
@@ -36,12 +38,16 @@ class CameraPreview : TextureView {
         if (widthRatio == null || heightRatio == null) {
             setMeasuredDimension(width, height)
         } else {
+            Log.d(this::class.simpleName, "current " +width+ " : " +height)
             val fixedHeight = width * heightRatio!! / widthRatio!!
             val fixedWidth = height * widthRatio!! / heightRatio!!
+
             if (width < fixedWidth) {
+                Log.d(this::class.simpleName, "fixed to " +width+ " : " +fixedHeight)
                 setMeasuredDimension(width, fixedHeight)
             }
             else {
+                Log.d(this::class.simpleName, "fixed to " +fixedWidth+ " : " +height)
                 setMeasuredDimension(fixedWidth, height)
             }
         }
--- app/src/main/java/jp/ac/titech/itpro/sdl/eyelevelcamera/MainActivity.kt
+++ app/src/main/java/jp/ac/titech/itpro/sdl/eyelevelcamera/MainActivity.kt
@@ -2,14 +2,20 @@ package jp.ac.titech.itpro.sdl.eyelevelcamera
 
 import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.Camera
+import android.graphics.Matrix
+import android.graphics.Rect
 import android.graphics.SurfaceTexture
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.hardware.SensorManager.AXIS_X
+import android.hardware.SensorManager.AXIS_Z
 import android.hardware.camera2.*
 import android.media.ImageReader
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
-import android.os.PersistableBundle
 import android.util.DisplayMetrics
 import android.util.Log
 import android.util.Size
@@ -22,14 +28,26 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.util.*
-import java.util.jar.Manifest
 
-class MainActivity : AppCompatActivity() {
-    private val TAG: String = "EYE_LEVEL_CAMERA"
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private val TAG = this::class.simpleName
     private val ACTIVITY_STATUS: String = "activity_status"
     private val REQUEST_CAMERA = 200
     private val REQUEST_STORAGE = 201
+
+    //sensor
+    private lateinit var eyeLevelView: EyeLevelView
+    private lateinit var sensorManager: SensorManager
+    private lateinit var accelerometer: Sensor
+    private lateinit var magnetometer: Sensor
+    private val accelerometerValue = FloatArray(3)
+    private val magnetometerValue = FloatArray(3)
+    private val rotationMatrix = FloatArray(9)
+    private val orientation = FloatArray(3)
+
+    //camera
     private var windowRoration: Int? = null
+    private var cameraRotation: Int? = null
     private lateinit var metrics: DisplayMetrics
 
     private var mCaptureSession: CameraCaptureSession? = null
@@ -39,9 +57,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mCameraId: String
     private var backThread : HandlerThread? = null
     private var backHandler : Handler? = null
-    private var frontThread : HandlerThread? = null
 
-    private var frontHandler: Handler? = null
     private var mCamera: CameraDevice? = null
     private val cameraManager by lazy {
         this.getSystemService(Context.CAMERA_SERVICE) as CameraManager
@@ -56,7 +72,8 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) {
-//                TODO("Not yet implemented")
+                Log.d(TAG, "onSurfaceTextureSizeChanged")
+                eyeLevelView.followSize(preview)
             }
 
             override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean = true
@@ -92,6 +109,13 @@ class MainActivity : AppCompatActivity() {
         }
     private val mCaptureCallback : CameraCaptureSession.CaptureCallback =
         object : CameraCaptureSession.CaptureCallback () {
+            override fun onCaptureCompleted(
+                session: CameraCaptureSession,
+                request: CaptureRequest,
+                result: TotalCaptureResult
+            ) {
+                super.onCaptureCompleted(session, request, result)
+            }
 
         }
     private val mOnImageAvailableListener : ImageReader.OnImageAvailableListener? = null
@@ -101,22 +125,34 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onCreate: ")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        preview = findViewById(R.id.preview)
-        val windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
-        windowRoration = windowManager.defaultDisplay.rotation
-        metrics = this.resources.displayMetrics
-        requestPermission()
-        preview.surfaceTextureListener = previewListener
-
         if (savedInstanceState != null) {
             activityStopped = savedInstanceState.getBoolean(ACTIVITY_STATUS)
         }
+        val windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
+        windowRoration = windowManager.defaultDisplay.rotation
+
+        //sensor
+        eyeLevelView = findViewById(R.id.eyeLevelView)
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        if (sensorManager == null) {
+            finish()
+            return
+        }
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+
+        //camera
+        preview = findViewById(R.id.preview)
+        preview.surfaceTextureListener = previewListener
+        metrics = this.resources.displayMetrics
+        requestPermission()
+
 
         // 撮影ボタン
         val button = findViewById<Button>(R.id.photo_button)
         button.setOnClickListener(fun(view: View) {
-//            mCameraId = cameraManager.cameraIdList[0]
-//            this.openCamera()
+            mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AF_TRIGGER, CameraMetadata.CONTROL_AF_TRIGGER_START)
+            mCaptureSession?.capture(mPreviewRequestBuilder.build(), mCaptureCallback, backHandler)
         })
     }
 
@@ -128,14 +164,16 @@ class MainActivity : AppCompatActivity() {
             openCamera()
             activityStopped = false
         }
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME)
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_GAME)
     }
 
-    // バックグラウンドになったらカメラを切る
     override fun onPause() {
         Log.d(TAG, "onPause: ")
         super.onPause()
         closeCamera()
         activityStopped = true
+        sensorManager.unregisterListener(this)
     }
 
     override fun onDestroy() {
@@ -171,6 +209,25 @@ class MainActivity : AppCompatActivity() {
         outState.putBoolean(ACTIVITY_STATUS, activityStopped)
     }
 
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerometerValue, 0, accelerometerValue.size)
+        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, magnetometerValue, 0, magnetometerValue.size)
+        }
+        val rawRotationMatrix = FloatArray(9)
+        SensorManager.getRotationMatrix(rawRotationMatrix, null, accelerometerValue, magnetometerValue)
+//      X-Z平面が基準面となるように変換
+        SensorManager.remapCoordinateSystem(rawRotationMatrix, SensorManager.AXIS_X, SensorManager.AXIS_Z, rotationMatrix)
+        SensorManager.getOrientation(rotationMatrix, orientation)
+        val timestamp = event.timestamp
+//            Log.d(TAG, "X: " +eventValueX+ ", Y: " +eventValueY+ ", Z:" +eventValueZ)
+        eyeLevelView.updateEyeLevel(orientation, timestamp)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
     private fun requestPermission() {
         val camera_permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
         val save_permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
@@ -184,7 +241,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun openCamera() {
         mCameraId = selectCameraId()
-        configureOptimalPreview(mCameraId)
+        val characteristics = cameraManager.getCameraCharacteristics(mCameraId)
+        configureView(characteristics)
         cameraManager.openCamera(mCameraId, mCameraCallback, backHandler)
     }
 
@@ -224,15 +282,30 @@ class MainActivity : AppCompatActivity() {
         return selectedId
     }
 
-    private fun configureOptimalPreview(id: String) {
-//        select optimal size of preview
-//        set aspect ratio
-        val characteristics = cameraManager.getCameraCharacteristics(id)
-        val map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
-        if (map != null) {
-            val availableSizes = map.getOutputSizes(SurfaceTexture::class.java)
-            val optimalSize = selectOptimalSize(availableSizes)
-            preview.setAspectRatio(optimalSize.getWidth(), optimalSize.getHeight())
+    private fun configureView(characteristics: CameraCharacteristics) {
+        cameraRotation = characteristics.get(CameraCharacteristics.SENSOR_ORIENTATION)
+        Log.d(TAG, "orientation " +cameraRotation)
+        val focalLength = characteristics.get(CameraCharacteristics.LENS_INFO_AVAILABLE_FOCAL_LENGTHS)
+        val pixelArraySize = characteristics.get(CameraCharacteristics.SENSOR_INFO_PIXEL_ARRAY_SIZE)
+        val activeArraySize = characteristics.get(CameraCharacteristics.SENSOR_INFO_ACTIVE_ARRAY_SIZE)
+        val physicalSize = characteristics.get(CameraCharacteristics.SENSOR_INFO_PHYSICAL_SIZE)
+        val pixelWidth = pixelArraySize?.width
+        val pixelHeight = pixelArraySize?.height
+        val activeWidth = activeArraySize?.width()
+        val activeHeight = activeArraySize?.height()
+        val physicalWidth = physicalSize?.width
+        val physicalHeight = physicalSize?.height
+
+        // preview
+        val matrix = Matrix()
+        matrix.postRotate(90*windowRoration!!.toFloat(), preview.width * 0.5f, preview.height * 0.5f)
+//            preview.setTransform(matrix)
+        if ((cameraRotation!! + 90*windowRoration!!) % 180 == 0) {
+            preview.setAspectRatio(activeWidth, activeHeight)
+            eyeLevelView.setAngle(focalLength!![0], physicalHeight!! * activeHeight!! / pixelHeight!!)
+        } else {
+            preview.setAspectRatio(activeHeight, activeWidth)
+            eyeLevelView.setAngle(focalLength!![0], physicalWidth!! * activeWidth!! / pixelWidth!!)
         }
     }
 
